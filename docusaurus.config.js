013261d45c71b92cc684e2df15f666766db667ee
emiliep <63355855+Mikoteko@users.noreply.github.com>
2023-10-05 10:52:19 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,115 +1,51 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const baseUrl = process.env.BASE_URL || '/';
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'Bus Sémantique',
-  tagline: 'Documentation du Bus Sémantique',
+module.exports = {
+  title: 'Semantic Bus',
+  tagline: 'Documentation of the Semantic Bus',
   url: 'https://assemblee-virtuelle.github.io',
-  baseUrl,
-  trailingSlash: false,
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  organizationName: 'assemblee-virtuelle', // Replace with your GitHub username
+  projectName: 'semantic-bus-documentation', // Replace with your GitHub repository name
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'assemblee-virtuelle', // Usually your GitHub org/user name.
-  projectName: 'semantic-bus-documentation', // Usually your repo name.
-  deploymentBranch: 'main',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr'],
+  themeConfig: {
+    navbar: {
+      title: 'Semantic Bus',
+      logo: {
+        alt: 'Semantic Bus logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        // ...Other navigation items
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        // ...Footer links
+      ],
+    },
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-        },
-        blog: {
-          showReadingTime: true,
+          // Other options for the documentation
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Bus Sémantique',
-        logo: {
-          alt: 'logo du bus sémantique',
-          src: 'img/logo-grappe.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/assemblee-virtuelle/semantic-bus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discourse (French)',
-                href: 'https://forums.assemblee-virtuelle.org/c/projets/bus-semantique/',
-              }
-            ],
-          },
-          {
-            title: 'Social',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/assemblee-virtuelle/semantic-bus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Virtual Assembly. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
 };
-
-module.exports = config;
