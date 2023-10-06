50625d3c33cdcd0fd98759b92ce8fd1da00cc677
epacheco <emiliepacheco@yahoo.com>
2023-10-06 11:15:07 +0200

    changed docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,6 +12,14 @@ module.exports = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
   themeConfig: {
     navbar: {
       title: 'Semantic Bus',
@@ -20,12 +28,34 @@ module.exports = {
         src: 'img/logo.svg',
       },
       items: [
+        // {
+        //   to: '/docs',
+        //   // activeBasePath: 'docs',
+        //   label: 'Docs',
+        //   position: 'left',
+        // },
+        // {
+        //   to: '/blog', 
+        //   label: 'Blog', 
+        //   position: 'left'
+        // },
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
           position: 'left',
+          label: 'Tutorial',
         },
+        {
+          to: '/blog', 
+          label: 'Blog', 
+          position: 'left'
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+
         // ...Other navigation items
       ],
     },
@@ -39,16 +69,38 @@ module.exports = {
 
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Other options for the documentation
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
+      }),
     ],
   ],
+
+  // presets: [
+  //   [
+  //     '@docusaurus/preset-classic',
+  //     {
+  //       docs: {
+  //         sidebarPath: require.resolve('./sidebars.js'),
+  //         // Other options for the documentation
+  //       },
+  //       theme: {
+  //         customCss: require.resolve('./src/css/custom.css'),
+  //       },
+  //     },
+  //   ],
+  // ],
 };
